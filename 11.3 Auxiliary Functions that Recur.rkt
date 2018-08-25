7580b3fe687a1dae0d91f13d68171df32959718d
Ayman Osman <aymano.osman@gmail.com>
2018-08-25 20:53:52 +0100

    wip

--- 11.3 Auxiliary Functions that Recur.rkt
+++ 11.3 Auxiliary Functions that Recur.rkt
@@ -2,3 +2,27 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |11.3 Auxiliary Functions that Recur|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;;; 11.3 Auxiliary Functions that Recur
+
+; List-of-numbers -> List-of-numbers
+; rearranges alon in descending order
+
+(check-expect (sort> '()) '())
+(check-expect (sort> (list 3 2 1)) (list 3 2 1))
+(check-expect (sort> (list 1 2 3)) (list 3 2 1))
+(check-expect (sort> (list 12 20 -5))
+              (list 20 12 -5))
+
+(define (sort> alon)
+  (cond
+    [(empty? alon) '()]
+    [else
+     (insert (first alon) (sort> (rest alon)))]))
+
+; Number List-of-numbers -> List-of-numbers
+; inserts n into the sorted list of numbers alon
+
+(check-expect (insert 5 '()) (list 5))
+(check-expect (insert 5 (list 6)) (list 6 5))
+(check-expect (insert 5 (list 4)) (list 5 4))
+
+(define (insert n alon) alon)
