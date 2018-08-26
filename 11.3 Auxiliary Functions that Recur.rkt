c64befd1690be8145f7050878641f6a11e97582e
Ayman Osman <aymano.osman@gmail.com>
2018-08-26 17:00:25 +0100

    wip

--- 11.3 Auxiliary Functions that Recur.rkt
+++ 11.3 Auxiliary Functions that Recur.rkt
@@ -24,5 +24,93 @@
 (check-expect (insert 5 '()) (list 5))
 (check-expect (insert 5 (list 6)) (list 6 5))
 (check-expect (insert 5 (list 4)) (list 5 4))
+(check-expect (insert 12 (list 20 -5))
+              (list 20 12 -5))
+(check-expect (insert 2 (list 3 2 1))
+              (list 3 2 2 1))
 
-(define (insert n alon) alon)
+(define (insert n alon)
+  (cond
+    [(empty? alon) (list n)]
+    [(cons? alon)
+     (if (>= n (first alon))
+         (cons n alon)
+         (cons (first alon) (insert n (rest alon))))]))
+
+;;; Exercise 186. Use sorted>? from exercise 145 to reformulate the tests
+;;; for sort> with check-satisfied.
+
+;; Part 1
+(check-satisfied (sort> (list 1 2 3)) sorted>?)
+
+;; From exercise 145
+; NEList-of-numbers -> Boolean
+(define (sorted>? l)
+  (cond
+    [(empty? (rest l)) #true]
+    [else
+     (and (>= (first l) (first (rest l)))
+          (sorted>? (rest l)))]))
+
+;; Part 2
+; List-of-numbers -> List-of-numbers
+; produces a sorted version of l
+(define (sort>/bad l)
+  (list 9 8 7 6 5 4 3 2 1 0))
+
+(check-satisfied (cons (sort>/bad (list 1 2 3))
+                       (list 1 2 3))
+                 not-sorted>?)
+
+; (cons List-of-numbers List-of-number) -> Boolean
+; (cons sorted-list original-list)
+(define (not-sorted>? stuff)
+  (not
+   (sorted-conditions (first stuff) (rest stuff))))
+
+; List-of-numbers List-of-numbers -> Boolean
+; applies a series of predicates to the two lists
+(define (sorted-conditions sl l)
+  (and
+   (= (length l) (length sl)) ; same-length?
+   #true; same-elements?
+   ))
+
+
+;;; Exercise 187. Design a program that sorts lists of game players by score:
+;;; Hint Formulate a function that compares two elements of GamePlayer.
+
+(define-struct gp [name score])
+; A GamePlayer is a structure:
+;    (make-gp String Number)
+; interpretation (make-gp p s) represents player p who
+; scored a maximum of s points
+
+(define gp3 (make-gp "gp3" 30))
+(define gp2 (make-gp "gp2" 20))
+(define gp1 (make-gp "gp1" 10))
+
+; List-of-GamePlayer -> List-of-GamePlayer
+
+(check-expect (sort-gp> (list gp1 gp3 gp2))
+              (list gp3 gp2 gp1))
+(define (sort-gp> l)
+  (cond
+    [(empty? l) '()]
+    [else
+     (insert-gp (first l) (sort-gp> (rest l)))]))
+
+; GamePlayer List-of-GamePlayer -> List-of-GamePlayer
+; insert p into sorted sl
+(define (insert-gp p sl)
+  (cond
+    [(empty? sl) (list p)]
+    [else
+     (if (gp> p (first sl))
+         (cons p sl)
+         (cons (first sl) (insert-gp p (rest sl))))]))
+
+; GamePlayer GamePlayer -> Boolean
+; p's score is greater than q's
+(define (gp> p q)
+  (> (gp-score p) (gp-score q)))
